70a0bd38f4f6d250f202cb2a5f6bcf1c1b7056c5
akasakakona <43999515+akasakakona@users.noreply.github.com>
2020-05-08 20:49:11 -0700

    Update ydltest.py

--- ydltest.py
+++ ydltest.py
@@ -1,10 +1,10 @@
-import youtube_dl
+import os
 
 youtube_dl_opts = {}
 
-url = input("Enter url: ")
-with youtube_dl.YoutubeDL(youtube_dl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        name = info_dict.get('title', None)
-        duration = info_dict.get('duration', None)
-print(f"Name: {name}\nDuration: {duration * 10}")
+url = input("Enter filename: ")
+for file in os.listdir("./"):
+        if(file.startswith(url)):
+                filename = file
+                print(filename)
+                break
