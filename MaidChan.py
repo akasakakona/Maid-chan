83f393710935036acfc44ada8104fd1e74658ddd
AnonymousBluejay <anonymousjayblue@gmail.com>
2021-04-14 23:25:37 -0700

    Encapsulated MaidChan into a class

---
+++ MaidChan.py
@@ -0,0 +1,38 @@
+import discord
+import os
+import json
+from discord.ext import commands
+
+
+class MaidChan(commands.Bot):
+    __TOKEN = ""
+    __PREFIX = ""
+
+    def __init__(self):
+        try:
+            with open('config.json') as f:
+                config_dict = json.load(f)
+            self.TOKEN = config_dict['TOKEN']
+            self.PREFIX = config_dict['PREFIX']
+        except FileNotFoundError:
+            print(f"MAID ERROR: \'config.json\' NOT FOUND UNDER CURRENT DIRECTORY: {os.getcwd()}")
+        # Create the bot
+        super().__init__(command_prefix=commands.when_mentioned_or(self.PREFIX), intents=discord.Intents.all());
+        self.load_extensions()
+        pass
+
+    def load_extensions(self):
+        for extension in os.listdir('./extensions'):  # load extensions
+            if extension.endswith('.py') and extension != "music.py":
+                self.load_extension(f'extensions.{extension[:-3]}')
+        pass
+
+    def run(self):
+        super().run(self, self.TOKEN)
+        pass
+
+    def get_token(self):
+        return self.TOKEN
+
+    def get_prefix(self):
+        return self.PREFIX
