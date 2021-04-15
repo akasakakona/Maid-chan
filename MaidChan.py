7ff76ec10e6868a8ea526a09eaa68abc566b777e
AnonymousBluejay <anonymousjayblue@gmail.com>
2021-04-15 01:41:31 -0700

    Update MaidChan.py

--- MaidChan.py
+++ MaidChan.py
@@ -30,7 +30,7 @@ class MaidChan(commands.Bot):
                     super().load_extension(f'extensions.{extension[:-3]}')
                 except Exception as e:
                     print(f"{extension} Could not be loaded!")
-                    e.with_traceback()
+                    print(e.__traceback__)
                 else:
                     print("{} has been loaded".format(extension))
         pass
