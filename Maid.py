23b724d87507af4ac32852857e62bf2f74fa86d9
AnonymousBluejay <anonymousjayblue@gmail.com>
2021-04-15 02:10:34 -0700

    Update Maid.py

--- Maid.py
+++ Maid.py
@@ -7,13 +7,13 @@ class Maid(commands.Bot):
 
     @staticmethod
     def instance():
-        if Maid.__instance is None:
+        if Maid.__instance is not MaidChan:
             print("Maid Chan instance is None, creating instance")
             Maid()
         return Maid.__instance
 
     def __init__(self):
-        if Maid.__instance is not None:
+        if Maid.__instance is MaidChan:
             raise Exception("This class is a singleton!")
         else:
             Maid.__instance = MaidChan()
