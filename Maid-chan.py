4ebca377b3bbf22034083b09459791e28222b46c
AnonymousBluejay <anonymousjayblue@gmail.com>
2021-04-14 23:39:04 -0700

    Revert "Revert "Encapsulated MaidChan into a class""
    This reverts commit fde5a0a64bd852480927a3c2a0abb31a5679fa0a.

--- Maid-chan.py
+++ Maid-chan.py
@@ -1,22 +1,5 @@
-import discord
-from discord.ext import commands
-import os
-import json
+import MaidChan
 
-TOKEN = ""
-PREFIX = ""
-try:
-    with open('config.json') as f:
-        config_dict = json.load(f)
-    TOKEN = config_dict['TOKEN']
-    PREFIX = config_dict['PREFIX']
-except FileNotFoundError:
-    print(f"MAID ERROR: \'config.json\' NOT FOUND UNDER CURRENT DIRECTORY: {os.getcwd()}")
 
-maid = commands.Bot(command_prefix=commands.when_mentioned_or(PREFIX), intents=discord.Intents.all())
-
-for extension in os.listdir('./extensions'):  # load extensions
-    if extension.endswith('.py') and extension != "music.py":
-        maid.load_extension(f'extensions.{extension[:-3]}')
-
-maid.run(TOKEN)
+maid = MaidChan()
+maid.run()
